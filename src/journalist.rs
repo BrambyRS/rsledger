389f9a001307d3781a8795d35d4ed824736f2cdc
Ranbir Singh <ranbir1998@gmail.com>
2026-03-12 19:33:13 +0000

    refactor: formalise double entry and transaction amount classes

--- src/journalist.rs
+++ src/journalist.rs
@@ -1,4 +1,5 @@
 mod input_parser;
+mod double_entry;
 
 use std::fs;
 use std::io;
@@ -23,6 +24,9 @@ pub fn new_journal(args: &Args) -> std::io::Result<()> {
     return Ok(());
 }
 
+/*
+Add entry to journal file
+*/
 // TODO: Input validation, error handling, multi currency support, multi entry support, etc.
 pub fn add_entry(args: &Args) -> std::io::Result<()> {
     // Get Journal path
@@ -31,17 +35,45 @@ pub fn add_entry(args: &Args) -> std::io::Result<()> {
         return Err(io::Error::new(io::ErrorKind::NotFound, format!("Journal file {} not found.", journal_file.display())));
     }
     
-    let date = input_parser::prompt_input("Date (YYYY-MM-DD): ")?;
-    let description = input_parser::prompt_input("Description: ")?;
-    let from_account = input_parser::prompt_input("From Account: ")?;
-    let amount_from = input_parser::prompt_input("Amount: ")?;
-    let to_account = input_parser::prompt_input("To Account: ")?;
-    let amount_to = input_parser::prompt_input("Amount: ")?;
+    let date_str: String = input_parser::prompt_input("Date (YYYY-MM-DD): ")?;
+    let description_str: String = input_parser::prompt_input("Description: ")?;
+    let account_from_str: String = input_parser::prompt_input("From Account: ")?;
+    let amount_from_str: String = input_parser::prompt_input("Amount: ")?;
+    let account_to_str: String = input_parser::prompt_input("To Account: ")?;
+    let amount_to_str: String = input_parser::prompt_input("Amount: ")?;
+
+    let amount_from = match double_entry::TransactionAmount::from_str(&amount_from_str) {
+        Some(val) => val,
+        None => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid amount format for 'From Account'.")),
+    };
+
+    // If it's empty, we can assume it's the negative of the amount from the 'From Account'.
+    let amount_to: double_entry::TransactionAmount;
+    if amount_to_str.len() == 0 {
+        amount_to = -amount_from.clone();
+    } else {
+        amount_to = match double_entry::TransactionAmount::from_str(&amount_to_str) {
+            Some(val) => val,
+            None => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid amount format for 'To Account'.")),
+        };
+
+        // If the currency is the same, validate that the amount is the negative of the amount from the 'From Account'.
+        if amount_to.same_currency(&amount_from) && !amount_to.same_amount(&(-amount_from.clone())) {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "Amount for 'To Account' must be the negative of the amount from 'From Account' if the currency is the same."));
+        }
+    };
+
+    let entry: double_entry::DoubleEntry = double_entry::DoubleEntry::new(
+        date_str,
+        description_str,
+        account_from_str,
+        amount_from,
+        account_to_str,
+        amount_to);
 
     // Append entry to journal file
-    let entry: String = format!("{date} {description}\n\t{from_account} {amount_from}\n\t{to_account} {amount_to}\n\n");
     fs::OpenOptions::new().append(true).open(journal_file)?;
-    fs::write(journal_file, entry)?;
+    fs::write(journal_file, format!("{entry}"))?;
 
     Ok(())
 }
---
+++ src/journalist/double_entry.rs
@@ -0,0 +1,107 @@
+/*
+TransactionAmount struct
+*/
+#[derive(Clone)]
+pub struct TransactionAmount {
+    amount: i32, // We save the amount as an integer (100x the amount in the journal) to avoid floating point issues.
+    currency: String,
+}
+
+impl core::fmt::Display for TransactionAmount {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        // Format the amount as a decimal with 2 places by placing a decimal point before the last two digits.
+        let amount_str = format!("{}.{:02}", self.amount / 100, self.amount.abs() % 100);
+        write!(f, "{} {}", amount_str, self.currency)
+    }
+}
+
+impl std::ops::Neg for TransactionAmount {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        TransactionAmount {
+            amount: -self.amount,
+            currency: self.currency,
+        }
+    }
+}
+
+impl PartialEq for TransactionAmount {
+    fn eq(&self, other: &Self) -> bool {
+        self.amount == other.amount && self.currency == other.currency
+    }
+}
+
+impl TransactionAmount {
+    pub fn from_str(amount_str: &str) -> Option<Self> {
+        // Split the amount string into the numeric part and the currency part.
+        let parts: Vec<&str> = amount_str.split_whitespace().collect();
+        if parts.len() != 2 {
+            return None;
+        }
+
+        let amount_part = parts[0];
+        let currency_part = parts[1].to_string();
+
+        // Remove the decimal point from the amount part and convert it to an integer.
+        // If there is no decimal point, we can just parse it as an integer and multiply by 100.
+        let amount_int: i32;
+        if amount_part.contains('.') {
+            let amount_int_str = amount_part.replace('.', "");
+            amount_int = match amount_int_str.parse::<i32>() {
+                Ok(val) => val,
+                Err(_) => return None,
+            };
+        } else {
+            let whole_part = match amount_part.parse::<i32>() {
+                Ok(val) => val,
+                Err(_) => return None,
+            };
+            amount_int = whole_part * 100; // Multiply by 100
+        }
+
+        Some(TransactionAmount {
+            amount: amount_int,
+            currency: currency_part,
+        })
+    }
+
+    pub fn same_currency(&self, other: &Self) -> bool {
+        self.currency == other.currency
+    }
+
+    pub fn same_amount(&self, other: &Self) -> bool {
+        self.amount == other.amount
+    }
+}
+
+/*
+DoubleEntry struct
+*/
+pub struct DoubleEntry {
+    date: String,
+    description: String,
+    account_from: String,
+    amount_from: TransactionAmount,
+    account_to: String,
+    amount_to: TransactionAmount,
+}
+
+impl core::fmt::Display for DoubleEntry {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{} {}\n\t{} {}\n\t{} {}\n\n", self.date, self.description, self.account_from, self.amount_from, self.account_to, self.amount_to)
+    }
+}
+
+impl DoubleEntry {
+    pub fn new (date: String, description: String, account_from: String, amount_from: TransactionAmount, account_to: String, amount_to: TransactionAmount) -> Self {
+        DoubleEntry {
+            date,
+            description,
+            account_from,
+            amount_from,
+            account_to,
+            amount_to,
+        }
+    }
+}
