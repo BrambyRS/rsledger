c4389161b644ff305512b3886f92bf895c8a6b9f
Ranbir Singh <ranbir1998@gmail.com>
2026-03-18 19:21:44 +0000

    feat: add Posting struct

--- src/journalist/transaction.rs
+++ src/journalist/transaction.rs
@@ -1,20 +1,45 @@
 pub mod commodity_value;
 
-/*
-Transaction struct
-*/
+pub struct Posting {
+    account: String,
+    amount: commodity_value::CommodityValue,
+}
+
+impl core::fmt::Display for Posting {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{} {}", self.account, self.amount)
+    }
+}
+
+impl Posting {
+    pub fn new(account: String, amount: commodity_value::CommodityValue) -> Self {
+        Posting {
+            account,
+            amount,
+        }
+    }
+}
+
+/// Represents a financial transaction with a date, description, and multiple posts (account and amount pairs).
 pub struct Transaction {
+    /// Date of the transaction in YYYY-MM-DD format.
     date: String,
+    /// Description of the transaction.
     description: String,
-    account_1: String,
-    amount_1: commodity_value::CommodityValue,
-    account_2: String,
-    amount_2: commodity_value::CommodityValue,
+    /// Account and amount pairs. For a simple double-entry transaction, there would be two posts with opposite amounts.
+    posts: Vec<Posting>,
 }
 
 impl core::fmt::Display for Transaction {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        write!(f, "{} {}\n\t{} {}\n\t{} {}\n\n", self.date, self.description, self.account_1, self.amount_1, self.account_2, self.amount_2)
+        write!(f, "{} {}\n", self.date, self.description)?;
+        for post in &self.posts {
+            match write!(f, "\t{}\n", post) {
+                Ok(_) => {},
+                Err(e) => return Err(e),
+            }
+        }
+        write!(f, "\n")
     }
 }
 
@@ -23,10 +48,10 @@ impl Transaction {
         Transaction {
             date,
             description,
-            account_1,
-            amount_1,
-            account_2,
-            amount_2,
+            posts: vec![
+                Posting::new(account_1, amount_1),
+                Posting::new(account_2, amount_2),
+            ],
         }
     }
 }
