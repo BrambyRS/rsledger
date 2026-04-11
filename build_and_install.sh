a642d6ee7e72676f7b571b3107d39f1522653e60
Ranbir Singh <ranbir1998@gmail.com>
2026-04-11 15:54:22 +0100

    feat: test before install

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,2 +1,7 @@
-cargo build --release
+#!/bin/bash
+set -e
+
+# Build and test the project
+cargo test --release
+# If tests pass, install the binary to /opt/rsledger
 mv target/release/rsledger /opt/rsledger
