11bfbf2d56326f04a654c8ee86632e58bdd493d6
Ranbir Singh <ranbir1998@gmail.com>
2026-08-27 19:44:03 +0100

    feat(importer): add trait for importers

--- src/csv_importer/mod.rs
+++ src/csv_importer/mod.rs
@@ -11,6 +11,12 @@ enum ImportCandidate<T: Display + Hash> {
     Unclassified(T),
 }
 
+/// Defines the IO-behaviour of an entry importer, allowing the underlying
+/// importing logic to be arbitrarily complex as long as it adheres to this interface.
+trait EntryImporter<T: Display + Hash> {
+    fn import_csv(&self, csv_path: std::path::PathBuf) -> Vec<ImportCandidate<T>>;
+}
+
 /// Import items into the journal file.
 /// This function handles deduplication of items and promps for manual classification where needed.
 fn import_items<T: Display + Hash>(
